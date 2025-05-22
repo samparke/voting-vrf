2c24f4c8c526ca8f736a446efde2d07c206516cc
Samuel Parke <samueljparke@gmail.com>
2025-05-22 06:50:26 +0100

    Ignored frontend folder

--- frontend/src/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useState, useRef, useEffect } from "react";
-import "../styles/Header.css";
-
-function Header() {
-  return <header className="header" style={{ padding: "30px" }}></header>;
-}
-
-export default Header;
--- frontend/src/utils/constants.js
+++
@@ -1,5 +0,0 @@
-import Vote from "./Vote.json";
-
-// our main vote contract abi and address
-export const voteContractAbi = Vote.abi;
-export const voteContractAddress = "0xee94c76bf8bc21341bebfdf1e059135d7dcc06c4";
